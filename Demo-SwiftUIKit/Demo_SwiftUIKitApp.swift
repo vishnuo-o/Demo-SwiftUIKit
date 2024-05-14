59ae3b82d0726433c019ef32b7141f20ed98cc98
i-steve <mickyvishnu007@gmail.com>
2024-05-14 21:59:58 +0530

    Initial Commit

---
+++ Demo-SwiftUIKit/Demo_SwiftUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUIKitApp.swift
+//  Demo-SwiftUIKit
+//
+//  Created by Vishnu Priyan on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
